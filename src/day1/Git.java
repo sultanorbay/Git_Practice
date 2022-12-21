4b0eb1c83b2caffd6721b6d1da96900c711de150
Sultan Orbay <orbaysultan@gmail.com>
2022-12-21 19:45:27 +0300

    fixed second

--- src/day1/Git.java
+++ src/day1/Git.java
@@ -4,7 +4,7 @@ public class Git {
     public static void main(String[] args) {
 
         System.out.println("Git in market");
-        System.out.println("Gitub on web");
+        System.out.println("Github on web");
 
 
 
