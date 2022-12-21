3b5ac4e2f5f5e98b8b8a63ea1d3a9ce4a01934ba
Sultan Orbay <orbaysultan@gmail.com>
2022-12-21 20:06:14 +0300

    Added

--- src/day1/Git_Hub.java
+++ src/day1/Git_Hub.java
@@ -4,6 +4,6 @@ public class Git_Hub {
     public static void main(String[] args) {
 
         System.out.println("Hello Git-Hub");
-
+        System.out.println("Local Repo is already published to GitHub");
     }
 }
